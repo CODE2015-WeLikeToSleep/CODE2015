7f9adf992ec4fae3276a8ab420e0c48090c6e119
Nic Waller <code@nicwaller.com>
2015-02-21 23:08:58 -0800

    use newly available date and description fields

--- Model.ChamberVoteDetail.js
+++ Model.ChamberVoteDetail.js
@@ -52,6 +52,8 @@ ChamberVoteDetail.prototype.fetchAsync = function(callback) {
 		url: 'votedata.php?p='+this.nthParliament+'&s='+this.nthSession+'&v='+this.nthVote,
 		success: function(dataJSON){
 			this.decision = dataJSON.decision;
+			this.date = dataJSON.date;
+			this.description = dataJSON.title;
 			dataJSON.details.forEach(function(value,ix,array){
 				self.votes.push(new PartyVoteCount(value.party,VoteType.YEA,value['yea']));
 				self.votes.push(new PartyVoteCount(value.party,VoteType.NAY,value['nay']));
